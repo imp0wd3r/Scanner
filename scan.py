a400f03bc7469ba1c6b8b99439aa45f7eef8e081
imp0wd3r <im.p0wd3r@gmail.com>
2018-01-03 15:41:33 +0800

    UPDATE scan.py

--- scan.py
+++ scan.py
@@ -14,6 +14,46 @@ from scanner.libs.result import save_result
 from scanner.utils import std_url
 
 
+def _vuln_scan(args):
+    """Vuln scan"""
+
+    patch_requests(args)
+
+    urls = []
+    if args.url:
+        urls.append(std_url(args.url))
+    else:
+        with open(args.url_file, 'r') as f:
+            for url in f:
+                urls.append(std_url(url.strip()))
+        urls = list(set(urls))
+
+    plugins = get_plugins(path=args.plugin_directory)
+
+    if args.plugin:
+        if os.path.dirname(args.plugin):
+            plugins = get_plugins(path=os.path.dirname(args.plugin))
+            targets = get_targets(urls, [os.path.basename(args.plugin)])
+        else:
+            targets = get_targets(urls, [args.plugin])
+    else:
+        targets = get_targets(urls, plugins.list_plugins())
+
+    result = run_threads(targets, plugins, args)
+
+    return result
+
+
+def _port_scan(args):
+    """Port scan"""
+
+    ms = Masscan(args)
+    ms.scan()
+    result = ms.parse_result_xml()
+
+    return result
+
+
 def main():
     """Entry function"""
 
@@ -21,35 +61,9 @@ def main():
     args = parse_cmd_options()
     
     if args.pattern == 'vuln':
-        # Vuln scan
-        patch_requests(args)
-
-        urls = []
-        if args.url:
-            urls.append(std_url(args.url))
-        else:
-            with open(args.url_file, 'r') as f:
-                for url in f:
-                    urls.append(std_url(url.strip()))
-            urls = list(set(urls))
-
-        plugins = get_plugins(path=args.plugin_directory)
-
-        if args.plugin:
-            if os.path.dirname(args.plugin):
-                plugins = get_plugins(path=os.path.dirname(args.plugin))
-                targets = get_targets(urls, [os.path.basename(args.plugin)])
-            else:
-                targets = get_targets(urls, [args.plugin])
-        else:
-            targets = get_targets(urls, plugins.list_plugins())
-
-        result = run_threads(targets, plugins, args)
+        result = _vuln_scan(args)
     else:
-        # Port scan
-        ms = Masscan(args)
-        ms.scan()
-        result = ms.parse_result_xml()
+        result = _port_scan(args)
     
     save_result(args.pattern, result, args.output, args.db)
 

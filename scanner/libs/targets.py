b34d45c2f1d049bad614af52da7c0e676e2e1212
imp0wd3r <im.p0wd3r@gmail.com>
2018-01-17 22:06:20 +0800

    FIX sens target

--- scanner/libs/targets.py
+++ scanner/libs/targets.py
@@ -9,8 +9,8 @@ def get_targets(pattern, urls, plugins=None, wordlist=None):
                 targets.append({'url': url, 'plugin': plugin})
     else:
         with open(wordlist, 'r') as f:
-            for url in urls:
-                for sens in f:
+            for sens in f:
+                for url in urls:
                     targets.append({'url': url, 'sens': sens.strip()})
     
     return targets
